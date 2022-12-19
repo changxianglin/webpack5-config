a32bae164e00e3a2d99caf136e2b21ab101929a5
changxianglin <281532612@qq.com>
2022-12-19 16:30:46 +0800

    add dynamic mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path')
 
+let mode = 'development'
+
+if(process.env.NODE_ENV === 'production') {
+  mode = 'production'
+}
+
 module.exports = {
-  mode: 'development',
-  devtool: false,
+  mode: mode,
+  devtool: "source-map",
 
   devServer: {
     static: {
